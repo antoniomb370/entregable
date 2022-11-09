2a890d6670c86b5e0dea3c98d49a1721cad9bd16
antoniomb370 <antonioobarragan370@gmail.com>
2022-11-08 19:52:41 -0500

    parcial

---
+++ src/clase10/Components/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {  Header} from "../Componets";
+
+
+function   Header =(props)=> {
+    const [count, setCount] = useState(0)
+  
+    return (
+      <div className="App">
+      <h1>holaa</h1>
+       
+      </div>
+    )
+  }
+  
+  export default Header
---
+++ src/clase10/Components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Header } from "../Componets";
+import productos from "../Componets"
+
+const Home = ()=>{
+
+const texto = "Home"
+return(
+
+    <div>
+
+<Header titulo={testo} descripcion="Esta es la ventana"/>
+{
+    prompt.map((Element)=><card Element={Element}/>)}
+
+    </div>
+)
+
+}
+
+export default Home
